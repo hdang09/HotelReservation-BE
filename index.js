d6439018f2ac3aeb36315626a1a5ea9e2ba9a7cf
HaiDang <dangtranhai628@gmail.com>
2022-08-26 09:26:30 +0700

    Update

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 const URI = process.env.DATABASE_URL;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, limit: '30MB' }));
-app.use('/', cors());
+app.use(cors());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use('/rooms', rooms);
 
